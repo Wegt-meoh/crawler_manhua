b6fdb7a97cdeb12f7336e479f2ee177e47312dc1
Wegt-meoh <pp1726636370@outlook.com>
2024-10-20 18:26:21 +0800

    feat: resume download

--- main.js
+++ main.js
@@ -23,64 +23,68 @@ const isAscending = await page.$$eval('.fed-drop-head a', anchors => anchors.red
     return prevValue + anchor.innerText
 }, '').includes('正序') ? true : false)
 
-let anchors = await page.$$eval('.all_data_list a', anchors => anchors.map(a => ({ title: a.title, href: a.href })));
+const anchors = await page.$$eval('.all_data_list a', anchors => anchors.map(a => ({ title: a.title, href: a.href })));
 if (isAscending) {
     anchors.reverse()
 }
 
-// test
-anchors = anchors.slice(201, 206)
+const resume = 214;
 
 // main work
-for (let i = 0; i < anchors.length; i++) {
-    await page.goto(anchors[i].href, { referer: target, waitUntil: 'networkidle0' })
-    await page.addScriptTag({ path: path.resolve('browser.js') })
+for (let i = resume; i < anchors.length; i++) {
+    try {
+        await page.goto(anchors[i].href, { referer: target, waitUntil: 'networkidle0' })
+        await page.addScriptTag({ path: path.resolve('browser.js') })
 
+        let prevScrollHeight = -10
+        while (true) {
+            await page.evaluate(() => window.scrollTo({ left: 0, top: document.body.scrollHeight }))
 
-    let prevScrollHeight = -10
-    while (true) {
-        await page.evaluate(() => window.scrollTo({ left: 0, top: document.body.scrollHeight }))
+            try {
+                await page.waitForNetworkIdle()
+            } catch {
+                prevScrollHeight = -10
+                await page.reload()
+                continue
+            }
 
-        try {
-            await page.waitForNetworkIdle()
-        } catch {
-            prevScrollHeight = -10
-            await page.reload()
-            continue
+            await sleep(300)
+
+            const scrollHeight = await page.evaluate(() => {
+                return document.body.scrollHeight;  // Total height of the page                                       
+            });
+
+            if (prevScrollHeight === scrollHeight) {
+                break
+            }
+
+            prevScrollHeight = scrollHeight
         }
 
-        await sleep(300)
+        const imageList = await page.evaluate(async () => {
+            const nameAndDataList = []
+            const imageUrlList = Array.from(document.querySelectorAll('.mh_comicpic img[src]')).map(n => n.src)
+            const imageNameList = imageUrlList.map(url => url.split('/').pop())
 
-        const scrollHeight = await page.evaluate(() => {
-            return document.body.scrollHeight;  // Total height of the page                                       
-        });
+            for (let i = 0; i < imageUrlList.length; i++) {
+                const response = await window.fetchWithTimeout(imageUrlList[i])
+                const arrayBuffer = await response.arrayBuffer()
 
-        if (prevScrollHeight === scrollHeight) {
-            break
-        }
+                nameAndDataList.push({ name: imageNameList[i], data: Array.from(new Uint8Array(arrayBuffer)) })
+            }
 
-        prevScrollHeight = scrollHeight
+            return nameAndDataList
+        })
+
+        const dowloadDir = path.join(outputDir, path.basename(target), i + '_' + anchors[i].title);
+        await fs.mkdir(dowloadDir, { recursive: true })
+        await Promise.all(imageList.map(image => fs.writeFile(path.join(dowloadDir, image.name), Buffer.from(image.data))))
+        await sleep(getRandomIntInRange(5, 10) * 1000)
+    } catch (error) {
+        console.error(error)
+        i -= 1
     }
 
-    const imageList = await page.evaluate(async () => {
-        const nameAndDataList = []
-        const imageUrlList = Array.from(document.querySelectorAll('.mh_comicpic img[src]')).map(n => n.src)
-        const imageNameList = imageUrlList.map(url => url.split('/').pop())
-
-        for (let i = 0; i < imageUrlList.length; i++) {
-            const response = await window.fetchWithTimeout(imageUrlList[i])
-            const arrayBuffer = await response.arrayBuffer()
-
-            nameAndDataList.push({ name: imageNameList[i], data: Array.from(new Uint8Array(arrayBuffer)) })
-        }
-
-        return nameAndDataList
-    })
-
-    const dowloadDir = path.join(outputDir, path.basename(target), i + '_' + anchors[i].title);
-    await fs.mkdir(dowloadDir, { recursive: true })
-    await Promise.all(imageList.map(image => fs.writeFile(path.join(dowloadDir, image.name), Buffer.from(image.data))))
-    await sleep(getRandomIntInRange(5, 10) * 1000)
 }
 
 //finish
